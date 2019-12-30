c3eed57b3524f277ba6c1a891dd9483996f3cce0
Mohammad <mike@mikey.ir>
2019-12-30 08:23:55 +0330

    Base implementation of Social login.

--- app/Http/Controllers/SocialAuth.php
+++ app/Http/Controllers/SocialAuth.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Providers\RouteServiceProvider;
 
 use Socialite;
 
@@ -10,14 +14,29 @@ class SocialAuth extends Controller
 {
     public function redirectToProvider()
     {
-      return Socialite::driver('google')->redirect();
+      return Socialite::driver('google')->redirect();  // TODO add phpdoc.
     }
+
     public function handleProviderCallback()
     {
-      $user = Socialite::driver('google')->stateless()->user();
-      print($user->token);
-      print($user->getName());
-      print($user->getEmail());
-      print($user->getAvatar());
+      $gUser = Socialite::driver('google')->user();
+
+      $user = $this->updateOrCreateUser($gUser);
+
+      Auth::login($user, true); // Log the user in and remember them without password.
+      return redirect(RouteServiceProvider::HOME);
+    }
+
+    private function updateOrCreateUser($user){
+      $dbUser = User::updateOrCreate(
+        ['email' => $user->getEmail()],
+        ['name' => $user->getName()]
+      );
+      if(!$dbUser->email_verified_at) // Email needs to be verified once only.
+      {
+        $dbUser->email_verified_at = Carbon::now();
+        $dbUser->save();
+      }
+      return $dbUser;
     }
 }
