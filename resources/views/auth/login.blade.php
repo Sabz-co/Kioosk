bec364b33fdea799a775a5efc485da13be8a45d6
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-01-12 23:47:12 +0330

    Change background color of auth pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 
 <section class="w-full flex h-screen">
-    <div class="items-center justify-center hidden lg:flex w-1/2 h-20 bg-gray-200 min-h-full p-20">
+    <div class="items-center justify-center hidden lg:flex w-1/2 h-20 bg-silver-200 min-h-full p-20">
         <div>
             <h1 class="text-brown font-light text-4xl">کیوسک، کاتالوگ اجتماعی کتاب</h1>
             <p class="text-2xl">متنی در مورد کیوسک و خدماتی که ارائه می‌دهیم اینجا قرار می‌گیرد.</p>
         </div>
 
     </div>
-    <div class="flex items-center justify-center w-full lg:w-1/2 h-20 bg-gray-300  min-h-full">
+    <div class="flex items-center justify-center w-full lg:w-1/2 h-20 bg-silver-300  min-h-full">
 
         <div class="w-5/6 sm:w-2/3 md:w-1/2 mx-auto flex flex-col">
-            <input class="mb-3 bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="نام و نام خانوادگی">
+            <input class="mb-3 bg-white focus:outline-none rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="نام و نام خانوادگی">
 
-            <input class="mb-3 bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="ایمیل">
+            <input class="mb-3 bg-white focus:outline-none rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="ایمیل">
 
-            <input class="mb-3 bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="کلمه عبور">
+            <input class="mb-3 bg-white focus:outline-none rounded-lg py-2 px-4 block w-full appearance-none leading-normal focus:shadow" type="email" placeholder="کلمه عبور">
         
 
             <div class="flex flex-row">
