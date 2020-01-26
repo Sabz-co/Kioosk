7ef37d69d318bd676994bab11b27e05fd14df33d
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-01-26 12:11:03 +0330

    Added proper relationships to review model

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $appends = ['favoritesCount', 'isFavorited', 'modelName', 'isSubscribedTo', 'ownerName', 'replies'];
+
+
+    public function owner()
+    {
+      return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function book()
+    {
+      return $this->belongsTo(Book::class, 'book_id');
+    }
+
+
+    public function replies()
+    {
+      return $this->hasMany(Reply::class);
+    }
 }
