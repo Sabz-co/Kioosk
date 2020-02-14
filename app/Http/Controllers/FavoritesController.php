af76b350f1bd5244e1a33ae2b540ec6d47c4f41c
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-02-14 12:21:52 +0330

    Added an auth middleware to favorite controller

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function store(Comment $comment) 
     {
         return DB::table('favorites')->insert([
