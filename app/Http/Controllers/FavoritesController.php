819ad2fd8195abd12e5aa1b0e86e0929021a5cf5
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-02-14 12:16:47 +0330

    Created a store method

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
-    //
+    public function store(Comment $comment) 
+    {
+        return DB::table('favorites')->insert([
+            'user_id' => auth()->id(),
+            'favorited_id' => $comment->id,
+            'favorited_type' => get_class($comment)
+        ]);
+    }
 }
