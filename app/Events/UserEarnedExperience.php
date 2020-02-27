83a15c7e06f4a7b4dcaacacdd64e0a74b0319c64
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-02-27 12:58:59 +0330

    Deleted some extra methods and classes from our event

--- app/Events/UserEarnedExperience.php
+++ app/Events/UserEarnedExperience.php
@@ -2,17 +2,13 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class UserEarnedExperience
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     /**
      * Create a new event instance.
@@ -24,13 +20,5 @@ class UserEarnedExperience
         //
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
+ 
 }
