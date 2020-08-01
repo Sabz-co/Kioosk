628907050c124b95b9408e7e99b315f2db528ca5
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-08-02 01:08:46 +0430

    Added margin between dropdown parts

--- resources/js/components/ProfileDropdown.vue
+++ resources/js/components/ProfileDropdown.vue
@@ -3,7 +3,7 @@
         <button class="block mt-4 items-center justify-center rounded-full lg:mt-0 text-white h-8 w-8 hover:text-white hover:bg-gray-700 mr-3 xl:mr-5 border-2 border-white">
             <img src="/public/images/avatar.jpg" alt="" class=" rounded-full">
         </button>
-        <div class="w-48 bg-white rounded-lg">
+        <div class="mt-2 w-48 bg-white rounded-lg">
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">تنظیمات پروفایل</a>
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">پشتیبانی</a>
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">خروج</a>
