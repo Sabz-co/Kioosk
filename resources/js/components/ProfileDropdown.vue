412df0c0b3630c139e22218ebf1da1872659de4f
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-08-02 19:38:16 +0430

    Removed the overlay

--- resources/js/components/ProfileDropdown.vue
+++ resources/js/components/ProfileDropdown.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <div class="relative">
         <button @click="isOpen = !isOpen" class="relative z-10 block mt-4 items-center justify-center rounded-full lg:mt-0 text-white h-8 w-8 hover:text-white hover:bg-gray-700 mr-3 xl:mr-5 border-2 border-white">
             <img src="/images/avatar.jpg" alt="" class=" rounded-full">
@@ -10,6 +11,18 @@
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">خروج</a>
         </div>
     </div>
+
+    <div class="relative">
+
+        <button v-if="isOpen" @click="isOpen = false" tabindex="-1" class="fixed inset-0 w-full h-full cursor-default"></button>
+        <div v-if="isOpen" class="absolute mt-2 py-2 w-48 bg-white rounded-lg shadow-lg left-0">
+            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">تنظیمات پروفایل</a>
+            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">پشتیبانی</a>
+            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">خروج</a>
+        </div>
+    </div>
+</div>
+
 </template>
 
 <script>
